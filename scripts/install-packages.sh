f9a8803652b3339548cdf527b8e7dbecae5c3c8e
Vyeos <rudranpatel0980@gmail.com>
2025-11-26 02:27:05 +0530

    edit: up-donw

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/bash
 
-sudo pacman -Syu --noconfirm
 
 sudo pacman -S --noconfirm neovim git curl htop base-devel waybar rofi cliphist wl-clipboard ripgrep lazygit
 
@@ -10,6 +9,8 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
 
-pacman -Syu --needed --noconfirm hypridle  hyprlock dunst libnotify fastfetch ttf-firacode-nerd ttf-jetbrains-mono-nerd pamixer cliphist slurp grim github-cli ntfs-3g noto-fonts-emoji
+pacman -S --noconfirm hypridle  hyprlock dunst libnotify fastfetch ttf-firacode-nerd ttf-jetbrains-mono-nerd pamixer cliphist slurp grim github-cli ntfs-3g noto-fonts-emoji
 
-yay -Sy --noconfirm brave-bin dunst hyprpaper light swappy pulseaudio swaylock-effects-git hyprpicker-git 
+yay -S --noconfirm brave-bin dunst hyprpaper light swappy pulseaudio swaylock-effects-git hyprpicker-git 
+
+sudo pacman -Syu --noconfirm

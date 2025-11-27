3f6b287c378fc89b679d3bbaf6f26b251434590c
vyeos <rudranpatel0980@gmail.com>
2025-11-27 17:22:41 +0530

    add: things

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,14 +2,17 @@
 
 cd ~
 #git clone https://github.com/vyeos/dotfiles
-ln -s /home/vyeos/dotfiles/config/hypr /home/vyeos/.config/hypr
-ln -s /home/vyeos/dotfiles/config/nvim /home/vyeos/.config/nvim
-ln -s /home/vyeos/dotfiles/config/rofi /home/vyeos/.config/rofi
-ln -s /home/vyeos/dotfiles/config/waybar /home/vyeos/.config/waybar
+rm -rf /home/vyeos/.config/hypr
+ln -s /home/vyeos/dotfiles/config/hypr /home/vyeos/.config
+ln -s /home/vyeos/dotfiles/config/nvim /home/vyeos/.config
+ln -s /home/vyeos/dotfiles/config/rofi /home/vyeos/.config
+ln -s /home/vyeos/dotfiles/config/waybar /home/vyeos/.config
+ln -s /home/vyeos/dotfiles/config/dunst /home/vyeos/.config
+ln -s /home/vyeos/dotfiles/config/ghostty /home/vyeos/.config
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions 
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 rm -rf /home/vyeos/.zshrc
-ln -s /home/vyeos/dotfiles/config/.zshrc /home/vyeos/.zshrc
+ln -s /home/vyeos/dotfiles/config/.zshrc /home/vyeos/

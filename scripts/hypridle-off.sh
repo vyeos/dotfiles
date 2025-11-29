2a5e1481ccaffd00820a0e68490f3b1f17323ab2
Vyeos <rudranpatel0980@gmail.com>
2025-11-29 10:25:41 +0530

    edit: hypridle-brightness, lock, suspend

--- scripts/hypridle-off.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-killall hypridle
-notify-send "Session Started"
--- scripts/hypridle-on.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if pgrep -x "hypridle" > /dev/null; then
-    echo "Hypridle is already running."
-else
-    hypridle &
-    notify-send "Session End"
-fi

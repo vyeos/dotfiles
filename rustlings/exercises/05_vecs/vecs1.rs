d381f1f105d61b47788d7b79f2dcacd5c83144f0
vyeos <rudranpatel0980@gmail.com>
2025-12-06 14:39:10 +0530

    add: rustlings

---
+++ rustlings/exercises/05_vecs/vecs1.rs
@@ -0,0 +1,20 @@
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // Array
+    let v = a.to_vec();
+    (a, v)
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array_and_vec_similarity() {
+        let (a, v) = array_and_vec();
+        assert_eq!(a, *v);
+    }
+}
